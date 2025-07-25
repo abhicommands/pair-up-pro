170ff71e1af560bea740556bdf9a2225c6f5c7cf
abhicommands <ac.abhinav@outlook.com>
2025-07-25 09:04:53 -0400

    fixed many bugs added new features-2

--- src/components/DataInput.tsx
+++ src/components/DataInput.tsx
@@ -5,7 +5,7 @@ import { Card } from '@/components/ui/card';
 interface DataInputProps {
   title: string;
   placeholder: string;
-  onLoadData: (data: any[]) => void;
+  onLoadData: (data: unknown[]) => void;
   isLoaded: boolean;
 }
 
